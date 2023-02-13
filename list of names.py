9dcdf8b346b634c864cc7afb16a0bd2d91e787d5
upneetkaur41 <123930263+upneetkaur41@users.noreply.github.com>
2023-02-13 11:20:22 +0530

    Add files via upload

---
+++ list of names.py
@@ -0,0 +1,36 @@
+#taking 10 enteries of names to make a list
+
+list_of_names=[]
+names=input("Enter a name")
+list_of_names.append(names)
+
+names=input("Enter a name")
+list_of_names.append(names)
+
+names=input("Enter a name")
+list_of_names.append(names)
+
+names=input("Enter a name")
+list_of_names.append(names)
+
+names=input("Enter a name")
+list_of_names.append(names)
+
+names=input("Enter a name")
+list_of_names.append(names)
+
+names=input("Enter a name")
+list_of_names.append(names)
+
+names=input("Enter a name")
+list_of_names.append(names)
+
+names=input("Enter a name")
+list_of_names.append(names)
+
+names=input("Enter a name")
+list_of_names.append(names)
+
+#printing the list
+print("names entered are:", list_of_names ) 
+      
